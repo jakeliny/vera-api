91fb652ab5fd85985c00b7f45f697382c2ed6e91
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2025-09-06 17:47:59 -0300

    fix: add in memory implementation register on registros module

--- src/registros/registros.module.ts
+++ src/registros/registros.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RegistrosController } from './registros.controller';
 import { RegistrosService } from './registros.service';
+import { InMemoryRegistrosRepository } from './repositories/in-memory-registros.repository';
 
 @Module({
   controllers: [RegistrosController],
-  providers: [RegistrosService],
+  providers: [
+    RegistrosService,
+    {
+      provide: 'IRegistrosRepository',
+      useClass: InMemoryRegistrosRepository,
+    },
+  ],
 })
 export class RegistrosModule {}
